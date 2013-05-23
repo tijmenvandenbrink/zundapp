97ed8886c5271d8ed384a594653a93eb135bf39f
Tijmen van den Brink <tijmen.vandenbrink@gmail.com>
2013-05-23 17:42:25 +0200

    Added admin for AccessPointLoad

--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.reports.models import ClientSession
+from ..reports.models import ClientSession, AccessPointLoad
 
 
 class ClientSessionAdmin(admin.ModelAdmin):
@@ -11,4 +11,12 @@ class ClientSessionAdmin(admin.ModelAdmin):
                     'disassociation_time', 'vendor', 'ap_name', 'snr', 'rssi', 'status', )
     search_fields = ('client_username', 'client_ip_address', 'client_mac_address', 'ap_name')
 
+
+class AccessPointLoadAdmin(admin.ModelAdmin):
+    list_display = ('ap_name', 'timestamp', 'bandwidth_available', 'clients_mcs_0', 'clients_mcs_1', 'clients_mcs_2',
+                    'clients_mcs_3', 'clients_mcs_4', 'clients_mcs_5', 'clients_mcs_6', 'clients_mcs_7')
+    list_filter = ('ap_name', 'timestamp', )
+    search_fields = ('ap_name', )
+
 admin.site.register(ClientSession, ClientSessionAdmin)
+admin.site.register(AccessPointLoad, AccessPointLoadAdmin)
